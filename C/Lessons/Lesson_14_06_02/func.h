4318b01123d63a1176766f2e4ee5d38a16ebd402
Anatolyvgy <anatolyitstep@gmail.com>
2018-04-29 18:57:26 +0300

    functions

---
+++ C/Lessons/Lesson_14_06_02/func.h
@@ -0,0 +1,6 @@
+#pragma once
+//#define DAYS_COUNT 7
+
+float FindAverageTemperature(float temperatures[ ]);
+float FindMaximumTemperature(float temperatures[ ]);
+float FindMinimumTemperature(float temperatures[ ]);
