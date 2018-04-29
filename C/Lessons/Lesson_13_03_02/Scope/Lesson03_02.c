188a5c7068a0f4caa7c7ba5304cb4f2613b6b527
Anatolyvgy <anatolyitstep@gmail.com>
2018-04-29 18:31:25 +0300

    scope and life time

---
+++ C/Lessons/Lesson_13_03_02/Scope/Lesson03_02.c
@@ -0,0 +1,24 @@
+//область видимости и время жизни 1.44 foto 281 time 10.56
+//scope and lifetime of variables
+
+#include<stdio.h>
+#include"Random.h"
+
+int main( )
+{
+	printf("maxRandNumber = %i\n", maxRandNumber);
+	printf("myRand = %i\n", myRand( ));
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
